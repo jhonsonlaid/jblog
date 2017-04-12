b58819196f63626e3708705cd24d2b2fb47d2030
jhonson-lab-win <jhonsonlaid@gmail.com>
2017-04-12 17:15:44 +0800

    add markdown extension

--- blog/templatetags/custom_markdown.py
+++ blog/templatetags/custom_markdown.py
@@ -10,7 +10,9 @@ register = template.Library()
 @stringfilter
 def custom_markdown(value):
     return mark_safe(markdown.markdown(value,
-                              extensions = ['markdown.extensions.fenced_code',
+                              extensions = ['markdown.extensions.extra',
+											'markdown.extensions.toc',
+											'markdown.extensions.nl2br',
                                             'markdown.extensions.codehilite'],
                               safe_mode = True,
                               enable_attributes = False))

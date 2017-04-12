34be6f5404a7253261cf54b2888384b9ce88eae7
jhonson-lab-win <jhonsonlaid@gmail.com>
2017-04-12 18:11:43 +0800

    change markdown extension

--- blog/templatetags/custom_markdown.py
+++ blog/templatetags/custom_markdown.py
@@ -12,6 +12,7 @@ def custom_markdown(value):
     return mark_safe(markdown.markdown(value,
                               extensions = ['markdown.extensions.extra',
 											'markdown.extensions.toc',
+											'markdown.extensions.sane_lists',
 											'markdown.extensions.nl2br',
                                             'markdown.extensions.codehilite'],
                               safe_mode = True,

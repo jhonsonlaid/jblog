68e1f08d8a1a781a0c15327b30da6858a802b1eb
jhonson-lab-win <jhonsonlaid@gmail.com>
2017-04-13 20:03:45 +0800

    fix clustermap bug and add it to gallery

--- static/js/self/gallery.js
+++ static/js/self/gallery.js
@@ -3,7 +3,7 @@
 /* jhonsonlaid[At]gmail[d0t]com */
 
 //var back_color_list = new Array("#CCFFFF", "#CCFFCC", "#FFCCCC", "#99FFCC", "#CCCCCC", "#FFCCFF", "#FFCC99");
-var back_color_list = new Array("#99FFFF", "#99FFCC", "#CC99CC", "#CC9999", "#CCCC99", "#CC99FF", "#CCFFCC", "#CCFF99", "#CCCC99", "#FFCC99", "#FF99CC", "#FF99FF", "#FFCC99", "#FFCCFF", "#FF99FF", "#FFFF99");
+var back_color_list = new Array("#99FFFF", "#AAFFCC", "#CCCCFF", "#CCDDFF", "#CCFFCC", "#CCFFAA", "#CCCCCC", "#FFCCAA", "#FFAACC", "#FFCCFF", "#FFCCAA", "#FFCCFF", "#FFFFCC");
 
 //change modal img background color by random
 modal_img_list = document.getElementsByClassName("gallery-cell");
